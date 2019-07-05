62f0134c7372fe0e5202c5b805f1d13bf74187c7
忘那滴泪 <liu1044775469@163.com>
2019-07-05 09:45:54 +0800

    基础信息

---
+++ src/com/kirin/bean/UserBean.java
@@ -0,0 +1,51 @@
+package com.kirin.bean;
+
+public class UserBean {
+	
+	
+	private static final String id = "111111111";
+	
+	private String name;
+	
+	private int age;
+	
+	private int size;
+	
+
+	public static String getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public int getSize() {
+		return size;
+	}
+
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+
+
+}
